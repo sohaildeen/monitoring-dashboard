fcbc0cefb3729601ce88d6695890259520b64d30
sohail87 <sohaildeen73@gmail.com>
2018-03-03 21:49:29 +0000

    list sqs queues

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export default {
+  iamUser: {
+    ACCESS_KEY: "AKIAJ2MXNDCCFTKKYE6Q",
+    SECRET_ACCESS_KEY: "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",
+    REGION: "eu-west-1"
+  }
+};
--- src/containers/SQS/ViewQueues.js
+++ src/containers/SQS/ViewQueues.js
@@ -1,45 +1,65 @@
-import React, { Component } from 'react'
-import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import React, {Component} from 'react'
+import {PageHeader, ListGroup, ListGroupItem} from "react-bootstrap";
 import './ViewQueues.css';
+import AWS from 'aws-sdk';
+import config from "../../config";
+
 
 export default class SqsViewQueues extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      queues:[]
+      queues: []
     }
   }
   async componentDidMount() {
     try {
-      const results = await this.queues();
-      this.setState({ queues: results });
+      this.getQueues();
+      //console.log(`results = ${results}`)
+      //this.setState({queues: results});
     } catch (e) {
       alert(e);
     }
   }
-  
-  queues() {
-    return [];
+
+  async getQueues() {
+    AWS.config.accessKeyId = config.iamUser.ACCESS_KEY;
+    AWS.config.secretAccessKey = config.iamUser.SECRET_ACCESS_KEY;
+    AWS.config.region = config.iamUser.REGION;
+
+    var sqs = new AWS.SQS();
+    var params = {
+      QueueNamePrefix: ''
+    };
+    var self = this;
+    await sqs.listQueues(params, function (err, data) {
+      if (err) {
+        console.log(err, err.stack); // an error occurred
+      } else {
+        console.log(data)
+        console.log(data.QueueUrls)
+        
+        self.setState({queues: data.QueueUrls});
+      }
+    });
+
   }
   renderQueuesList(queues) {
-    return [{}].concat(queues).map(
-      (queue, i) =>
-          <ListGroupItem
-              //key={queue.arn}
-              //href={`/sqs/queue/${queue.arn}`}
-              //onClick={this.handleQueueClick}
-              //header={queue.name.trim().split("\n")[0]}
-            >
-              {
-                //"Created: " + new Date(queue.createdAt).toLocaleString()
-              }
-            </ListGroupItem>    
-    );
+    return []
+      .concat(queues)
+      .map((queueUrl, i) => <ListGroupItem key={queueUrl} href={`/sqs/queue/${queueUrl}`} onClick={this.handleQueueClick} 
+      //header={queueUrl}
+      >
+        {queueUrl}
+      </ListGroupItem>);
   }
   handleQueueClick = event => {
     event.preventDefault();
-    this.props.history.push(event.currentTarget.getAttribute("href"));
+    this
+      .props
+      .history
+      .push(event.currentTarget.getAttribute("href"));
   }
   render() {
     return (
